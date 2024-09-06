da40ff020261f45b8fa374112a8ec508ce85debc
PEPPACHAN <PEPPACHAN@users.noreply.github.com>
2024-09-06 15:36:33 +0300

    Initial Commit

---
+++ FindByIP/FindByIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindByIPApp.swift
+//  FindByIP
+//
+//  Created by PEPPA CHAN on 06.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FindByIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
